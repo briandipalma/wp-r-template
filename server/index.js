ef1b73105568ed2f8d1c54ca0c642a97d8b3023f
Brian Di Palma <offler@gmail.com>
2015-09-03 09:43:44 +0100

    chore: add webpack hmr support to template

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
-var Server = require("socket.io");
+"use strict";
 
-var io = new Server(7070);
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
 
-io.on("connection", function(socket) {
+var config = require("../webpack.config");
+
+var webpackDevServer = new WebpackDevServer(webpack(config), {
+	publicPath: config.output.publicPath,
+	hot: true,
+	historyApiFallback: true
 });
+
+function logListenEvents(err, result) {
+	/* eslint-disable no-console */
+	if (err) {
+		console.log(err);
+	}
+
+	console.log("Listening at localhost:8080");
+	/* eslint-enable no-console */
+}
+
+webpackDevServer.listen(8080, "localhost", logListenEvents);
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,21 @@
 
 var join = require("path").join;
 
+var webpack = require("webpack");
+
 var buildOutputDir = join(__dirname, "dist");
 var appEntryPoint = join(__dirname, "app", "main.js");
 
 module.exports = {
-	entry: appEntryPoint,
+	entry: [
+		"webpack-dev-server/client?http://localhost:8080", // WebpackDevServer host and port
+		"webpack/hot/only-dev-server", // "only" prevents reload on syntax errors
+		appEntryPoint
+	],
 	output: {
 		path: buildOutputDir,
-		filename: "bundle.js"
+		filename: "bundle.js",
+		publicPath: "/dist/"
 	},
 	devtools: "eval",
 	module: {
@@ -20,6 +27,16 @@ module.exports = {
 			test: /\.js$/,
 			exclude: /node_modules/,
 			loader: "babel-loader"
+		}, {
+			test: /\-react\.js$/,
+			exclude: /node_modules/,
+			loaders: [
+				"react-hot",
+				"babel-loader"
+			]
 		}]
-	}
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	]
 };
