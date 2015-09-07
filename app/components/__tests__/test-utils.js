0a0eac0e8e83e95cc86c5d628ca5776c290848de
Brian Di Palma <offler@gmail.com>
2015-09-07 21:43:23 +0100

    fix: change tests to use non development babel config
    Move babel HMR config to development and change tests to use non existant
    `test` babel `env`. This means tests don't use HMR babel config and can
    run in node.
    
    Updated React addons imports as React addons is now deprecated.

--- app/components/__tests__/test-utils.js
+++ app/components/__tests__/test-utils.js
@@ -1,11 +1,10 @@
-import React, {addons} from "react/addons";
-
-const TestUtils = addons.TestUtils;
+import React, {createElement} from "react";
+import {createRenderer} from "react/lib/ReactTestUtils";
 
 export function createComponent(component, props, ...children) {
-	const shallowRenderer = TestUtils.createRenderer();
+	const shallowRenderer = createRenderer();
 
-	shallowRenderer.render(React.createElement(component, props, children.length > 1 ? children : children[0]));
+	shallowRenderer.render(createElement(component, props, children.length > 1 ? children : children[0]));
 
 	return shallowRenderer.getRenderOutput();
 }
