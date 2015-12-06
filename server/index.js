d5fffbfc56601df8856d0630003b618b50de5ca1
Brian Di Palma <offler@gmail.com>
2015-12-06 21:41:44 +0000

    feat: Server side render the index page

--- server/index.js
+++ server/index.js
@@ -1,43 +1,35 @@
 /* @flow */
 
-import {join} from "path";
-
 import express from "express";
 import webpack from "webpack";
 import webpackDevMiddleware from "webpack-dev-middleware";
 import webpackHotMiddleware from "webpack-hot-middleware";
 
 import config from "../webpack.config";
+import {indexRouteHandler} from "./indexPage-react";
 
 const APP_PORT = 8080;
 const devMiddlewareOptions = {
 	noInfo: true,
 	publicPath: config.output.publicPath
 };
-const indexPage = join(__dirname, "..", "index.html");
 
 const app = express();
 const compiler = webpack(config);
 const hmrMiddleware = webpackHotMiddleware(compiler);
 const devMiddleware = webpackDevMiddleware(compiler, devMiddlewareOptions);
 
-function allRoutesHandler(req, res) {
-	console.log("Request for", req.url); // eslint-disable-line
-
-	res.sendFile(indexPage);
-}
-
 function logListenEvents(err) {
 	if (err) {
 		console.log(err); // eslint-disable-line
+	} else {
+		console.log(`Listening at localhost:${APP_PORT}`); // eslint-disable-line
 	}
-
-	console.log("Listening at localhost:8080"); // eslint-disable-line
 }
 
 app.use(devMiddleware);
 app.use(hmrMiddleware);
 
-app.get("*", allRoutesHandler);
+app.get("/", indexRouteHandler);
 
 app.listen(APP_PORT, "localhost", logListenEvents);
---
+++ server/indexPage-react.js
@@ -0,0 +1,30 @@
+/* @flow */
+
+import express from "express";
+import React from "react";
+import {renderToStaticMarkup} from "react-dom/server";
+
+const IndexPage = () => {
+	return (
+		<html>
+			<head>
+				<meta charSet="utf-8"/>
+			</head>
+			<body>
+				<div id="container">
+				</div>
+				<script src="/static/bundle.js"></script>
+			</body>
+		</html>
+	);
+};
+
+export function indexRouteHandler(req: express.Request, res: express.Response) {
+	console.log(`Request for ${req.url}`); // eslint-disable-line
+
+	// JSX can't process DOCTYPE tags.
+	res.send(
+		`<!DOCTYPE html>
+		${renderToStaticMarkup(<IndexPage />)}`
+	);
+}
