3ff42911d1c8c3793d5cbdb0e0ed2e6d42751404
Brian Di Palma <offler@gmail.com>
2016-03-06 14:46:59 +0000

    refactor: Delete commented out socket.io code

--- app/main.js
+++ app/main.js
@@ -2,10 +2,8 @@
 
 import React from 'react';
 import {render} from 'react-dom';
-// import io from "socket.io-client";
 
 import './main-style.css';
 import App from './components/App-react';
 
-// const socket = io("http://localhost:7070/");
 render(<App name="Brian Di Palma" />, document.getElementById('container'));
